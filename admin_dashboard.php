3945d2731d0b94c53aa7cdd91a8312636bf044d7
John Philip Brimon <kyledomingo783@gmail.com>
2026-05-11 21:37:25 +0800

    latest update

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -98,11 +98,11 @@ unset($_SESSION['success'], $_SESSION['error']);
 
 $rfidPortalUrl = getenv('RFID_PORTAL_URL');
 if ($rfidPortalUrl === false || trim($rfidPortalUrl) === '') {
-    $rfidPortalUrl = 'http://10.147.237.184/';
+    $rfidPortalUrl = 'http://172.20.10.3/';
 }
 $rfidPortalUrl = rtrim((string) $rfidPortalUrl, '/') . '/';
 if (!filter_var($rfidPortalUrl, FILTER_VALIDATE_URL)) {
-    $rfidPortalUrl = 'http://10.147.237.184/';
+    $rfidPortalUrl = 'http://172.20.10.3/';
 }
 
 function adminTableExists(mysqli $conn, string $table): bool
